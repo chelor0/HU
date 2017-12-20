01fc66ffe1680b7be306d6d57083503e189156c4
Che Lor <che.lor@technekes.com>
2017-12-20 08:55:41 -0500

    New Repo for Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  #routes the root page to the home index page
+  root 'home#index'
+  
+  #routes the "about" module the home_controller.rb to show the "about" page from the views folder.
+  get '/about' => 'home#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
