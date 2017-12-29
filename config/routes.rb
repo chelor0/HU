c907e94ed3b664f8113ed9bef885bb9cc0fdfd3f
Che Lor <che.lor@technekes.com>
2017-12-29 00:14:55 -0500

    Work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :answers
   resources :questions
   #routes the root page to the home index page
   root 'home#index'
@@ -6,6 +7,9 @@ Rails.application.routes.draw do
   #routes the "about" module the home_controller.rb to show the "about" page from the views folder.
   get '/about' => 'home#about'
   
-  
+  post '/questions' => 'home#temp'
+  post '/answers' => 'home#temp'
+
+  get '/questions/:id' => 'home#question'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
